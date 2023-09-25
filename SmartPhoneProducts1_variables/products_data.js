86d90927e9860989e54cd47415b4c8843c4ce647
Wendy Huang <143070180+wendyh2@users.noreply.github.com>
2023-09-25 00:21:17 -1000

    pl

---
+++ SmartPhoneProducts1_variables/products_data.js
@@ -0,0 +1,21 @@
+let name1 = "HTC";
+let price1 = 40.00;
+let image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
+
+let name2 = "Apple";
+let price2 = 75.00;
+let image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
+
+let name3 = "Nokia";
+let price3 = 35.00;
+let image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
+
+let name4 = "Samsung";
+let price4 = 45.00;
+let image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
+
+let name5 = "Blackberry";
+let price5 = 10.00;
+let image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
+
+console.log("Product variables loaded...")
