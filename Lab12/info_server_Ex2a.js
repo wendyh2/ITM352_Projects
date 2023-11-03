5ada6880b4d102b3233910362ff3657d42499e82
wendyh <143070180+wendyh2@users.noreply.github.com>
2023-11-03 08:35:37 -1000

    stuff

---
+++ Lab12/info_server_Ex2a.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to path ' + request.path);
+    next();
+});
+
+app.get('/test', function (request, response, next) {
+    response.send('in route GET to /test');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
