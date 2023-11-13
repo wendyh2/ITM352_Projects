ea09a779f7b0f5269b6d6fe51116814e95c0c419
wendyh <143070180+wendyh2@users.noreply.github.com>
2023-11-12 23:02:07 -1000

    yu

---
+++ Huang_Wendy_Assignment1/server.js
@@ -0,0 +1,23 @@
+// Author: Wendy Huang
+// Date: 11/10/2023
+// My server js file that runs the server for my site
+
+const express = require('express');
+const app = express();
+
+// Routing 
+
+// monitor all requests
+app.all('*', function (request, response, next) {
+   console.log(request.method + ' to ' + request.path);
+   next();
+});
+
+// process purchase request (validate quantities, check quantity available)
+<** your code here ***>
+
+// route all other GET requests to files in public 
+app.use(express.static(__dirname + '/public'));
+
+// start server
+app.listen(8080, () => console.log(`listening on port 8080`));
