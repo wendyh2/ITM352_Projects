58e184d9b9f991f1c47d932e1bbd1fab7355224e
wendyh <143070180+wendyh2@users.noreply.github.com>
2023-11-19 20:47:54 -1000

    ji

--- Huang_Wendy_Assignment1/server.js
+++ Huang_Wendy_Assignment1/server.js
@@ -27,50 +27,53 @@ app.get("/products_data.js", function (request, response, next) {
 app.post("/purchase", function (request, response, next) {
     console.log(request.body);
 
-    const errors = {};
-    let hasQty = false;
-    let hasInput = false;
+    // validate quantities
+    const errors = {}; //assume no errors at start
+    let hasQty = false; // assuming user did not select any valid quantities/products
+    let hasInput = false; 
+    // validates data and loops it through our array in products.json file 
+    for (let i in products) {
+        const qty = request.body[`quantity${i}`];
 
-    for (const i in products) {
-        const qty = parseInt(request.body[`quantity${i}`]);
-
+        // Did the user select any products? 
         if (qty > 0) {
-            hasQty = true;
+            hasQty = true; //If they had quantity selected then 
             hasInput = true;
         }
-
+        //If they didn't have anything on the quantity box then assume they didn't select anything and put it as 0 
         if (qty == "") {
             request.body[`quantity${i}`] = 0;
         }
-
-        if (!findNonNegInt(qty)) {
+        //Validates the quantity and if it was a non-negative integer 
+        if (findNonNegInt(qty) === false) {
             errors[`quantity${i}_error`] = findNonNegInt(qty, true).join("<br>");
             hasInput = true;
         }
-
-        if (qty > products[i].quantity_available) {
-            errors[`quantity${i}_available_error`] = `We don't have ${qty} available!`;
+        // If the quantity selevcted is greater than the products available then send back an error 
+        if (qty > products[i].sets_available) {
+            errors[`quantity${i}_error`] = `We don't have ${qty} available!`;
             hasInput = true;
         }
     }
-
-    if (hasQty === false && hasInput === false) {
+    // If the there were no qunatities input then send back an error
+    if (hasQty === false) {
         errors[`noQty`] = `Please select some items to purchase!`;
     }
 
     console.log(errors);
-
+    // A loop, so when theres no errors at all the customers are send into the invoice 
     if (Object.keys(errors).length === 0) {
+        // When the purchase is valid this will reduce our inventory by the amounts purchased
         for (const i in products) {
-            products[i].quantity_available -= request.body[`quantity${i}`];
+            products[i].sets_available -= Number(request.body[`quantity${i}`]);
             products[i].quantity_sold += Number(request.body[`quantity${i}`]);
         }
+        // This redirects them to the invoice with a querystring that has the values they wanted and they've been removed from the inventory
         response.redirect("./invoice.html?" + querystring.stringify(request.body));
-    } else {
-        request.body["errorsJSONstring"] = JSON.stringify(errors);
+    } else { // This is if there were errors we wend them back to the products display and are notified of the problems 
         response.redirect(
-            "./products_display.html?" + querystring.stringify(request.body)
-        );
+            "./products_display.html?" + querystring.stringify(request.body) + "&" + querystring.stringify(errors) 
+        ); //We will be redirected to either a invoice page with the data we input if there are errors then we will be redirected to our products display with the errors we found
         console.log(request.body);
     }
 });
@@ -79,13 +82,13 @@ app.post("/purchase", function (request, response, next) {
 app.use(express.static(__dirname + '/public'));
 
 // Start server
-app.listen(8080, () => console.log(`listening on port 800`));
+app.listen(8080, () => console.log(`listening on port 8080`));
 
 function findNonNegInt(q, returnErrors = false) {
     const errors = [];
-    if (Number(q) !== q) errors.push("Not a number!");
+    if (Number(q) != q) errors.push("Not a number!");
     if (q < 0) errors.push("Negative value!");
-    if (parseInt(q) !== q) errors.push("Not an integer!");
+    if (parseInt(q) != q) errors.push("Not an integer!");
 
     return returnErrors ? errors : errors.length === 0;
 }
