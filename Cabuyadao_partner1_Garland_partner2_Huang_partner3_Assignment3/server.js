c95435a241eb2978bc769416b2516596ab936674
wendyh <143070180+wendyh2@users.noreply.github.com>
2023-12-13 18:33:55 -1000

    LOGOUT SUCCESS FOR THE INVOICE

--- Cabuyadao_partner1_Garland_partner2_Huang_partner3_Assignment3/server.js
+++ Cabuyadao_partner1_Garland_partner2_Huang_partner3_Assignment3/server.js
@@ -480,60 +480,47 @@ app.get('/checkout', (request, response) => {
         });
     });    
 
-    // Thank user for purchase and tell them they are logged out then redirect to home.html
-    response.send(`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap" rel="stylesheet">
-    <style>
-        body {
-            font-family: 'Montserrat', sans-serif;
-            background-image: url(img/topback.png);
-            background-position: center;
-            background-size: cover;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            margin: 0;
-        }
-        .message-container {
-            text-align: center;
-        }
-        .thank-you-message {
-            font-size: 30px; /* Adjusted font size to 30px */
-            margin-bottom: 10px;
-        }
-        .bold-red {
-            font-weight: bold;
-            color: red;
-        }
-        .logout-message {
-            font-size: 24px; /* Adjusted font size to 24px */
-            margin-bottom: 10px;
-        }
-    </style>
-</head>
-<body>
-    <div class="message-container">
-        <div class="thank-you-message">
-            Thank you for your purchase, <span class="bold-red">${user_registration_info[username].name}</span>.
-        </div>
-        <div class="logout-message">
-            Logged out ${username}.
-        </div>
-        <div>
-            You will be redirected to the home page in 5 seconds.
-        </div>
-    </div>
-    <meta http-equiv="refresh" content="5;url=home.html">
-</body>
-</html>
-`);
+  // Logout route that sends user to the thank you page and then logs out
+app.get('/logout', (req, res) => {
+    const username = req.session.username; // Assuming username is stored in the session
+
+    // Remove username from loggedInUsers object (logging the user out)
+    if (loggedInUsers.hasOwnProperty(username)) {
+        delete loggedInUsers[username];
+    }
+
+    // Prepare and send the thank you message
+    const thankYouMessage = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            ...
+        </head>
+        <body>
+            <div class="message-container">
+                <div class="thank-you-message">
+                    Thank you for your purchase, <span class="bold-red">${user_registration_info[username].name}</span>.
+                </div>
+                <div class="logout-message">
+                    Logged out ${username}.
+                </div>
+                <div>
+                    You will be redirected to the home page in 5 seconds.
+                </div>
+            </div>
+            <meta http-equiv="refresh" content="5;url=/home.html">
+        </body>
+        </html>
+    `;
+
+    // Send the thank you message and then destroy the session
+    res.send(thankYouMessage, () => {
+        // Destroy the session after sending the response
+        req.session.destroy();
+    });
 });
+
 
 // Serve static files
 app.use(express.static(__dirname + '/public'));
