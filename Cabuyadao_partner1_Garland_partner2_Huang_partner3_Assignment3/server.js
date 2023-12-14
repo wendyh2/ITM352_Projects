56ca61ba1d62db9b13b5077be57d5e1333334e6e
wendyh <143070180+wendyh2@users.noreply.github.com>
2023-12-13 18:51:12 -1000

    idk anymore

--- Cabuyadao_partner1_Garland_partner2_Huang_partner3_Assignment3/server.js
+++ Cabuyadao_partner1_Garland_partner2_Huang_partner3_Assignment3/server.js
@@ -495,7 +495,36 @@ app.get('/logout', (req, res) => {
         <html lang="en">
         <head>
             <meta charset="UTF-8">
-            ...
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap" rel="stylesheet">
+            <style>
+                body {
+                    font-family: 'Montserrat', sans-serif;
+                    background-image: url(img/topback.png);
+                    background-position: center;
+                    background-size: cover;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    height: 100vh;
+                    margin: 0;
+                }
+                .message-container {
+                    text-align: center;
+                }
+                .thank-you-message {
+                    font-size: 30px; /* Adjusted font size to 30px */
+                    margin-bottom: 10px;
+                }
+                .bold-red {
+                    font-weight: bold;
+                    color: red;
+                }
+                .logout-message {
+                    font-size: 24px; /* Adjusted font size to 24px */
+                    margin-bottom: 10px;
+                }
+            </style>
         </head>
         <body>
             <div class="message-container">
