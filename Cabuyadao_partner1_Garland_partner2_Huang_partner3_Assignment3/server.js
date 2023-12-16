de7691ba2a8ad273914cf364ca122af4f1af7876
gelliec <143069410+gelliec@users.noreply.github.com>
2023-12-16 10:54:26 +0900

    Quantity Desired Revert to 0 after add to cart

--- Cabuyadao_partner1_Garland_partner2_Huang_partner3_Assignment3/server.js
+++ Cabuyadao_partner1_Garland_partner2_Huang_partner3_Assignment3/server.js
@@ -314,7 +314,7 @@ app.post("/login", function (request, response, next) {
             // Regular user logic
             // send a usernames cookie to indicate they're logged in
             response.cookie("userinfo", JSON.stringify({ "email": username, "full_name": name }), { expire: Date.now() + 30 * 1000 });
-            // Keep track of the number of times a user logged in and the last time they logged in. 
+            // IR4 - Keep track of the number of times a user logged in and the last time they logged in. 
             user_registration_info[username].loginCount += 1;
             user_registration_info[username].lastLoginDate = Date.now();
 
@@ -423,16 +423,16 @@ app.post("/register", function (request, response, next) {
         // Save new registration info into user_registration_info.json
         user_registration_info[username] = {};
         user_registration_info[username].name = request.body.name;
-        // Store encrypted password into user_registration_info
+        // IR1: Store encrypted password into user_registration_info
         user_registration_info[username].password = hashPassword(request.body.password);
-        // Add lastLoginDate and loginCount for this new user make it a string
+        // IR4 add lastLoginDate and loginCount for this new user make it a string
         user_registration_info[username].lastLoginDate = Date.now();
         user_registration_info[username].loginCount = 1;
 
         // Write to our user_registration_info.json file, we add the null and 2 option to account for a null replacer, and indentation
         fs.writeFileSync(filename, JSON.stringify(user_registration_info, null, 2));
 
-        // Add username to keep track of amount of logged in users
+        // IR5: Add username to keep track of amount of logged in users
         // Check if loggedInUsers already has the username so that we don't login more than once for the same user
         if (!loggedInUsers.hasOwnProperty(username)) {
             loggedInUsers[username] = true; // You can use `true` to indicate that the user is logged in.
@@ -625,24 +625,9 @@ app.post('/admin/login', function (request, response) {
     if (isAdmin(username) && user_registration_info[username].password === hashPassword(password)) {
         request.session.user = username;
         request.session.isAdmin = true;
-        // Redirect to the choice page instead of directly to admin panel
-        response.redirect('/admin/choice');
+        response.send({ success: true });
     } else {
         response.send({ success: false, message: 'Invalid credentials or not an admin' });
-    }
-});
-
-// Route to display choice page for admin after login
-app.get('/admin/choice', function (request, response) {
-    if (request.session.isAdmin) {
-        // Serve a simple HTML page with choices
-        response.send(`
-            <h1>Welcome Admin</h1>
-            <p>Choose where you would like to go:</p>
-            <a href="/admin_panel.html">Admin Panel</a> | <a href="/products_display.html">User Interface</a>
-        `);
-    } else {
-        response.redirect('/login.html');
     }
 });
 
@@ -700,7 +685,6 @@ app.post('/admin/users', function (request, response) {
         case 'add':
             // Add a new user
             if (!user_registration_info[username]) {
-                userData.password = hashPassword(userData.password); // Hash the password
                 user_registration_info[username] = userData;
             } else {
                 return response.status(400).send('User already exists');
@@ -709,9 +693,6 @@ app.post('/admin/users', function (request, response) {
         case 'edit':
             // Edit an existing user
             if (user_registration_info[username]) {
-                if(userData.password) {
-                    userData.password = hashPassword(userData.password); // Hash the password if provided
-                }
                 user_registration_info[username] = { ...user_registration_info[username], ...userData };
             } else {
                 return response.status(404).send('User not found');
